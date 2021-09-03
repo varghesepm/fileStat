ba5281d10152a0d5f285b650d7ef16e195de9e94
Varghese P Mathew <33537762+varghesepm@users.noreply.github.com>
2021-09-03 17:06:30 +0530

    output formated

--- main.sh
+++ main.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-fParser() {
-	STAT=`stat -c "%U:%G %y %n" $1`
+# colorize output heading
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+# get file details and print
+statPrinter() {
+	STAT=`stat -c "\t\t|%U:%G \t\t|%.19z \t|%n" $1`
 	DU=`du -h $1 | awk {'print $1'}`
-	echo "$DU $STAT"
+	printf "$DU $STAT\n"
 }
 
-find ./ -type f -mtime -2  > /tmp/file20.txt
+# find files with mtime -15 and write its result into a file
+find ./ -type f -mtime -15  > /tmp/file20.txt
 
-
+# read file
 while read f; do
-	fParser $f
+	awk 'BEGIN{ printf "'${RED}'%s\t\t'${NC}'| '${RED}'%s\t\t\t'${NC}'| '${RED}'%s\t\t\t'${NC}'| '${RED}'%s'${NC}'","Size", "Owner","mtime","Filename"} END { print "" }'
+        statPrinter $f
 done < /tmp/file20.txt
