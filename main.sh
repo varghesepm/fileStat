d393d5135c07507b9b92c37fd1880b5e2d518593
Varghese P Mathew <33537762+varghesepm@users.noreply.github.com>
2021-09-04 17:59:10 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,7 +4,7 @@
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-# get file details and print
+# print file stats
 statPrinter() {
 	STAT=`stat -c "\t\t|%U:%G \t\t|%.19z \t|%n" $1`
 	DU=`du -h $1 | awk {'print $1'}`
@@ -14,8 +14,9 @@ statPrinter() {
 # find files with mtime -15 and write its result into a file
 find ./ -type f -mtime -15  > /tmp/file20.txt
 
+awk 'BEGIN{ printf "'${RED}'%s\t\t'${NC}'| '${RED}'%s\t\t\t'${NC}'| '${RED}'%s\t\t\t'${NC}'| '${RED}'%s'${NC}'\n","Size", "Owner","mtime","Filename"'}
+
 # read file
 while read f; do
-	awk 'BEGIN{ printf "'${RED}'%s\t\t'${NC}'| '${RED}'%s\t\t\t'${NC}'| '${RED}'%s\t\t\t'${NC}'| '${RED}'%s'${NC}'","Size", "Owner","mtime","Filename"} END { print "" }'
         statPrinter $f
 done < /tmp/file20.txt
